6af800ad44a48f18a64ff6f88e91a7844962de5d
Mohamad Kendi <99072874@mydavinci.nl>
2022-10-20 13:37:02 +0200

    dinogame

--- hello-python/dinoGame.py
+++ hello-python/dinoGame.py
@@ -1,57 +1,6 @@
-from operator import truediv
 
-
-print('welcome to dino game')
-story = 0
-while story == 0:
-    while True:
-        age = input('are you old enough (18) to play? (yes/no) ')
-        if age == 'no':
-            print('then what are you doing here? try again when u are old enough')
-            exit()
-        elif age == 'yes':
-            print('ur good to go')
-            story+=1
-        else :
-            print('try again!')
-        break
-    
-
-print('this story contains several bosses. to end the game you need to walk through the story and kill the minions and level up!')
-
-print('if u wanna proceed in the game u need to choose a weapon:')
-print('sword')
-print('shield')
-print('dagger')
-sword = 'sword'
-shield = 'shield'
-dagger = 'dagger'
-
-while story == 1:
-    while True:
-        weapon = input('wich one do u choose? ')
-
-        if  weapon in ["sword"]:
-            sword = print(f'oeeehhh sword let them fear the true samurai inside u ')
-            story+=1
-        elif weapon in ['shield']:
-            shield = print('shield is trash u will die')
-            story+=1
-        elif weapon in ['dagger']:
-            dagger = print('wise choice')
-            story+=1
-        else:
-            print('thats not mentioned')
-        break
-
-print('\n')
-##########################
-while story == 2:
-    while True:
-        begin = input('now you can begin fighting, this is a miniboss, do you want to proceed? (yes/no) ')
-        if begin == 'no':
-            print('try again when ur ready: BE A MAN')
-            print(''' ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+GAME_OVER='''
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
 ███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
 ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
 ██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
@@ -75,17 +24,8 @@ while story == 2:
 ┼┼┼┼┼┼┼┼▄▄▄██┼┼█▀█▀█┼┼██▄▄▄┼┼┼┼┼┼┼┼┼
 ┼┼┼┼┼┼┼┼▀▀██┼┼┼┼┼┼┼┼┼┼┼██▀▀┼┼┼┼┼┼┼┼┼
 ┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼ ''')
-            exit()
-        elif begin == 'yes':
-            print('may the gods protect you')
-            story+=1
-        else :
-            print('Wrong answer buddy')
-        break
-##########################
-
-print('''
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼ '''
+DINO1 = '''
                      /~~~~~~~~~~~~\_
  _+=+_             _[~  /~~~~~~~~~~~~\_
 {""|""}         [~~~    [~   /~~~~~~~~~\_
@@ -98,62 +38,8 @@ print('''
                 <"_V_">      \\  \    ~~~~~~\\  \
                               \\  \          \\  \
                               <"_V_">        <"_V_">
-''')
-while story == 3:
-    while True :
-        attack = input('press e to attack!')
-        if attack == 'e':
-            print('what a hit! nice job, you will proceed to the next level')
-            story+=1
-        else:
-            print('''
-        ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-        ███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
-        ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
-        ██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
-        ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
-        ███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
-        ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-        ███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
-        ██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
-        ██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
-        ██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
-        ███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
-        ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼████▄┼┼┼▄▄▄▄▄▄▄┼┼┼▄████┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼┼▀▀█▄█████████▄█▀▀┼┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼┼┼┼█████████████┼┼┼┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼┼┼┼██▀▀▀███▀▀▀██┼┼┼┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼┼┼┼██┼┼┼███┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼┼┼┼█████▀▄▀█████┼┼┼┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼┼┼┼┼███████████┼┼┼┼┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼▄▄▄██┼┼█▀█▀█┼┼██▄▄▄┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼▀▀██┼┼┼┼┼┼┼┼┼┼┼██▀▀┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼
-        ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-        you are trsah, you cant even press e. kleine speler
-        ''')
-            exit()
-        break
-
-print('\n')
-
-while story == 4:
-    while True :
-        way = input('''now you are in crossway, you have to choos to go left or right!
-        Where do you want to go? choose a direction! but u know there are consequences wich way you choose---___--- ''')
-        if way == 'left':
-            print('you are now in the jungle here you will play against the pro old sexy trex king. this king is not ez, you will struggle!')
-            ready = input('are you ready? yes/no')
-            if ready == 'yes':
-                print('let the game begin')
-                story+=1
-            elif ready == 'no':
-                print('yea no problem you will get over it just run!')
-            else:
-                print('thats is not asked! try again ')
-            print('''                                                     -- __
+'''
+DINO2 = '''                                                     -- __
                                                           ~ (@)  ~~~---_
                                                         {     `-_~=======)
                                                         {    (_  ',
@@ -175,41 +61,8 @@ while story == 4:
                          .=._________,'  )                     ))
                          )  _________ -''                     ~~
                         / /  _ _                   =
-                       (_.-.'O'-'. ''')
-            attack1 = input('press s to attack!')
-            if attack1 == "s":
-                print("that was a good hit, lesgoooo")
-            else :    
-                print('''that was not the right key-_- 
-                ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-                ███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
-                ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
-                ██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
-                ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
-                ███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
-                ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-                ███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
-                ██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
-                ██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
-                ██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
-                ███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
-                ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼████▄┼┼┼▄▄▄▄▄▄▄┼┼┼▄████┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼┼▀▀█▄█████████▄█▀▀┼┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼┼┼┼█████████████┼┼┼┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼┼┼┼██▀▀▀███▀▀▀██┼┼┼┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼┼┼┼██┼┼┼███┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼┼┼┼█████▀▄▀█████┼┼┼┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼┼┼┼┼███████████┼┼┼┼┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼▄▄▄██┼┼█▀█▀█┼┼██▄▄▄┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼▀▀██┼┼┼┼┼┼┼┼┼┼┼██▀▀┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼
-                ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼ ''')
-        elif way == 'right':
-            print('you are right, right is the right answer nou you can skip miniboss to go to the last boss!')
-            story+=1
-            print('''                         . - ~ ~ ~ - .
+                       (_.-.'O'-'. '''
+DINO3 = '''                         . - ~ ~ ~ - .
               ..     _      .-~               ~-.
              //|     \ `..~                      `.
             || |      }  }              /       \  \
@@ -218,33 +71,14 @@ while story == 4:
          (__          |       /        |       /      `.
           `- - ~ ~ -._|      /_ - ~ ~ ^|      /- _      `.
                       |     /          |     /     ~-.     ~- _
-                      |_____|          |_____|         ~ - . _ _~_-_''')
-            skip = input('press x to skip')
-            if skip == "x":
-                print('Now you are in the last fase, you have to defeat the last boss to win the game!')
-        else:
-            print('thats not a direction')
-        break
-        
-print('\n')
-
-
-
-#last boss
-lastFase = input('Hello mighty dino slayer, this is the last fase, now u will fase the true fear! press x to enter the room')
-if lastFase =="x":
-    print('''  __
+                      |_____|          |_____|         ~ - . _ _~_-_'''
+BOSS = '''  __
               / _)
      _.----._/ /
     /         /
  __/ (  | (  |
-/__.-'|_|--|_| ''')
-    attack2 = input('this is the true fear! press a to attack!')
-    if attack2 == 'a':
-        attack3 = input('hit him again!')
-        attack4 = input('once again!')
-        if attack4 == 'a':
-            print('''well played! this is your dinoslayer badge!                       _..----------.._                       
+/__.-'|_|--|_| '''
+BADGE = '''well played! this is your dinoslayer badge!                       _..----------.._                       
                   .-=""        _       ""=-.                  
                .-"    _.--""j _\""""--._    "-.               
             .-"  .-i   \   / / \;       ""--.  "-.            
@@ -280,62 +114,118 @@ if lastFase =="x":
                "-._                        _.-"               
                    """---...______...---"""  
                    I hope u have enjoyed playing this game!
-                   made by Mo kendi''')    
+                   made by Mo kendi'''
+print('welcome to dino game')
+story = 0
+while story == 0:
+        age = input('are you old enough (18) to play? (yes/no) ')
+        if age == 'no':
+            print('then what are you doing here? try again when u are old enough')
+            exit()
+        elif age == 'yes':
+            print('ur good to go')
+            story+=1
         else :
-            print(''' u had to press a
-┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
-██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
-██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
-██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
-███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
-┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
-██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
-██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
-██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
-███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
-┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼████▄┼┼┼▄▄▄▄▄▄▄┼┼┼▄████┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼▀▀█▄█████████▄█▀▀┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼█████████████┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼██▀▀▀███▀▀▀██┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼██┼┼┼███┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼█████▀▄▀█████┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼┼███████████┼┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼▄▄▄██┼┼█▀█▀█┼┼██▄▄▄┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼▀▀██┼┼┼┼┼┼┼┼┼┼┼██▀▀┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-you are trsah, you cant even press d. kleine speler
- ''')
+            print('try again!')
+    
+
+print('this story contains several bosses. to end the game you need to walk through the story and kill the minions and level up!')
+
+print('if u wanna proceed in the game u need to choose a weapon:')
+print('sword')
+print('shield')
+print('dagger')
+sword = 'sword'
+shield = 'shield'
+dagger = 'dagger'
+
+while story == 1:
+        weapon = input('wich one do u choose? ')
+
+        if  weapon in ["sword"]:
+            sword = print(f'oeeehhh sword let them fear the true samurai inside u ')
+            story+=1
+        elif weapon in ['shield']:
+            shield = print('shield is trash u will die')
+            story+=1
+        elif weapon in ['dagger']:
+            dagger = print('wise choice')
+            story+=1
+        else:
+            print('thats not mentioned')
+
+print('\n')
+##########################
+while story == 2:
+        begin = input('now you can begin fighting, this is a miniboss, do you want to proceed? (yes/no) ')
+        if begin == 'no':
+            print('try again when ur ready: BE A MAN')
+            print(GAME_OVER)
+            exit()
+        elif begin == 'yes':
+            print('may the gods protect you')
+            story+=1
+        else :
+            print('Wrong answer buddy')
+##########################
+
+print(DINO1)
+while story == 3:
+        attack = input('press e to attack!')
+        if attack == 'e':
+            print('what a hit! nice job, you will proceed to the next level')
+            story+=1
+        else:
+            print(GAME_OVER)
+            exit()
+
+print('\n')
+
+while story == 4:
+        way = input('''now you are in crossway, you have to choos to go left or right!
+        Where do you want to go? choose a direction! but u know there are consequences wich way you choose---___--- ''')
+        if way == 'left':
+            print('you are now in the jungle here you will play against the pro old sexy trex king. this king is not ez, you will struggle!')
+            ready = input('are you ready? yes/no')
+            if ready == 'yes':
+                print('let the game begin')
+                story+=1
+            elif ready == 'no':
+                print('yea no problem you will get over it just run!')
+            else:
+                print('thats is not asked! try again ')
+            print(DINO2)
+            attack1 = input('press s to attack!')
+            if attack1 == "s":
+                print("that was a good hit, lesgoooo")
+            else :    
+                print(GAME_OVER)
+        elif way == 'right':
+            print('you are right, right is the right answer nou you can skip miniboss to go to the last boss!')
+            story+=1
+            print(DINO3)
+            skip = input('press x to skip')
+            if skip == "x":
+                print('Now you are in the last fase, you have to defeat the last boss to win the game!')
+        else:
+            print('thats not a direction')
+
+        
+print('\n')
+
+
+
+#last boss
+lastFase = input('Hello mighty dino slayer, this is the last fase, now u will fase the true fear! press x to enter the room')
+if lastFase =="x":
+    print(BOSS)
+    attack2 = input('this is the true fear! press a to attack!')
+    if attack2 == 'a':
+        attack3 = input('hit him again!')
+        attack4 = input('once again!')
+        if attack4 == 'a':
+            print(BADGE)    
+        else :
+            print(GAME_OVER)
 else :
-    print('''
-┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
-██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
-██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
-██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
-███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
-┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
-██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
-██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
-██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
-███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
-┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼████▄┼┼┼▄▄▄▄▄▄▄┼┼┼▄████┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼▀▀█▄█████████▄█▀▀┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼█████████████┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼██▀▀▀███▀▀▀██┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼██┼┼┼███┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼█████▀▄▀█████┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼┼███████████┼┼┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼▄▄▄██┼┼█▀█▀█┼┼██▄▄▄┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼▀▀██┼┼┼┼┼┼┼┼┼┼┼██▀▀┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼
-┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
-you are trsah, you cant even press a. kleine speler
-''')
+    print(GAME_OVER)
