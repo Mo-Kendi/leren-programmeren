f199d4c5563a1d06a0a995e270efe6668c7b4c3c
Mohamad Kendi <99072874@mydavinci.nl>
2022-11-17 14:22:29 +0100

    while

---
+++ hello-python/blastoff.py
@@ -0,0 +1,7 @@
+a = 30
+
+while True:
+    print(f"{a}")
+    a -= 1
+    if a == 0:
+        break
---
+++ hello-python/while_tijd.py
@@ -0,0 +1,12 @@
+am = 1
+while True:
+    print(f'{am}:00 AM')
+    am += 1
+    if am ==13:
+        break
+pm = 1
+while True:
+    print(f"{pm}:00 PM")
+    pm += 1
+    if pm ==13:
+        break
