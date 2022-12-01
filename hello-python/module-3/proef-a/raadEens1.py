06b8f28d5e28d5c22482b98824cb5557af5e4782
Mohamad Kendi <99072874@mydavinci.nl>
2022-12-01 11:26:01 +0100

    verbetering in raad opdracht

--- hello-python/module-3/proef-a/raadEens1.py
+++ hello-python/module-3/proef-a/raadEens1.py
@@ -5,23 +5,25 @@ aantalpoginen = 10
 ronde = 0
 score = 0
 poging = 0
-raadGetal = random.randint(1, 1000)
 while ronde < aantalRounds:
     meeDoen = input("zou je een getal tussen 1 en 1000 willen raden? ja/nee  ")
-
+    raadGetal = random.randint(1, 1000) #randint staat voor een random int getal.
+    print(raadGetal)
     if meeDoen == "nee":
         print("einde")
         print(f"Jou eind score is {score} van de {aantalRounds} punten!")
         exit()
     elif meeDoen == "ja":
         print('je gaat nu 10 keer raden en dan kijken wij of je goed hebt ')
+    else:
+        print('vul ja of nee in')
 
     while poging < aantalpoginen:
         while True:
             try:
                 antwoord = int(input('raad het getal: '))
                 break
-            except ValueError:
+            except ValueError: #dit betekent dat als je iets andres dan een getal invoert moet je dan iets opnieuwe in vullen
                 print('je mag alleen cijfers')
 
         if raadGetal == antwoord:
@@ -34,6 +36,11 @@ while ronde < aantalRounds:
                 print("Je bent heel dichtbij!")
             elif verschil <= 50 and verschil >= 20:
                 print("Je bent dichtbij!")
+
+            if antwoord > raadGetal:
+                print('gok lager')
+            elif antwoord < raadGetal:
+                print('gok hoger!')
         poging += 1
 
     print(f"jou eind score is{score} ")
