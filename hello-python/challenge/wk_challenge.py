71c4f652b61ca05ded2e00cf8b3c93b064d0a3fd
Mohamad Kendi <99072874@mydavinci.nl>
2022-12-08 10:58:51 +0100

    wk_cahllenge opdracht 2

--- hello-python/challenge/wk_challenge.py
+++ hello-python/challenge/wk_challenge.py
@@ -1,5 +1,8 @@
 matchWin = 3
 matchLose = 0
+puntenTotaal1 = 0
+puntenTotaal2 = 0
+puntenTotaal3 = 0
 land1 = input ('welke land speelt eerst in groep A? ')
 land2 = input ('welke land speelt als tweede in groep A? ')
 land3 = input ('welke land speelt als derde in groep A? ')
@@ -12,3 +15,18 @@ wedstrijden     thuis       uit         Doelpunten1         Doelpunten2         
 2               {land2}     {land3}
 3               {land1}     {land3}''')
 
+
+doelsaldo1 = int(input(f'hoeveel doelpunten heeft {land1} gemaakt? '))
+doelsaldo2 = int(input(f'hoeveel doelpunten heeft {land2} gemaakt? '))
+if doelsaldo1 > doelsaldo2:
+    print(f'de winnaar is {land1}')
+    puntenTotaal1 += matchWin
+elif doelsaldo1 < doelsaldo2:
+    print(f'de winnaar is {land2}')
+    puntenTotaal2 += matchLose
+print(f''' 
+Land        Doelsaldo       Puntentotaal
+{land1}         {doelsaldo1}                        {puntenTotaal1}
+{land2}         {doelsaldo2}                        {puntenTotaal2}
+{land3}     
+''')
