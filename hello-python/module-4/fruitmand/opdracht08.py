663dbe85516713badea8781e862d913dc651819e
Mohamad Kendi <99072874@mydavinci.nl>
2022-12-16 11:51:53 +0100

    opdracht 8 ingeleverd

---
+++ hello-python/module-4/fruitmand/opdracht08.py
@@ -0,0 +1,14 @@
+from fruitmand import fruitmand
+fruitmand.append({
+'name': 'watermeloen', 
+'weight': 1300, 
+'color': 'lime', 
+'round' : True 
+})
+totaal = 0
+for fruit in fruitmand:
+    totaal = totaal + fruit['weight']
+print(totaal)
+
+
+    
