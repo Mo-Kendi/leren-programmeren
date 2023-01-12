d9cb2063fd7b7a44adf6adf3e93add3d294814d2
Mohamad Kendi <99072874@mydavinci.nl>
2023-01-12 09:47:28 +0100

    fruimand opdrachten inleveren

---
+++ hello-python/module-4/fruitmand/opdracht09.py
@@ -0,0 +1,4 @@
+from fruitmand import fruitmand
+fruitmand.pop(4)
+for fruit in fruitmand:
+    print (fruit['color'])
---
+++ hello-python/module-4/fruitmand/opdracht10.py
@@ -0,0 +1,16 @@
+from fruitmand import fruitmand
+fruitmand.pop(4)
+for fruit in fruitmand:
+    print (fruit['weight'])
+
+
+
+
+
+#from fruitmand import fruitmand
+#from operator import itemgetter
+#
+#fruitmand = sorted(fruitmand , key = itemgetter("weight"),reverse=True)
+#for x in fruitmand:
+#        print(x.get("weight"))
+
