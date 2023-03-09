3d0150e89c0491b1e0a8e90ec9dbafa6845e1df3
Mohamad Kendi <99072874@mydavinci.nl>
2023-03-09 13:03:20 +0100

    dino game functios toegevoegd en ingeleverd

--- hello-python/module-2/proefA/dinoGame.py
+++ hello-python/module-2/proefA/dinoGame.py
@@ -5,54 +5,5 @@ story = welcome()
 weapon = choose_weapon() 
 begin = ready_to_fight()
 attack = first_fight()
-
-print('\n')
-
-while story == 3:
-        way = input('''now you are in crossway, you have to choos to go left or right!
-        Where do you want to go? choose a direction! but u know there are consequences wich way you choose---___--- ''')
-        if way == 'left':
-            print('you are now in the jungle here you will play against the pro old sexy trex king. this king is not ez, you will struggle!')
-            ready = input('are you ready? yes/no')
-            if ready == 'yes':
-                print('let the game begin')
-                story+=1
-            elif ready == 'no':
-                print('yea no problem you will get over it just run!')
-            else:
-                print('thats is not asked! try again ')
-            print(DINO2)
-            attack1 = input('press s to attack!')
-            if attack1 == "s":
-                print("that was a good hit, lesgoooo")
-            else :    
-                print(GAME_OVER)
-        elif way == 'right':
-            print('you are right, right is the right answer nou you can skip miniboss to go to the last boss!')
-            story+=1
-            print(DINO3)
-            skip = input('press x to skip')
-            if skip == "x":
-                print('Now you are in the last fase, you have to defeat the last boss to win the game!')
-        else:
-            print('thats not a direction')
-
-        
-print('\n')
-
-
-
-#last boss
-lastFase = input('Hello mighty dino slayer, this is the last fase, now u will fase the true fear! press x to enter the room')
-if lastFase =="x":
-    print(BOSS)
-    attack2 = input('this is the true fear! press a to attack!')
-    if attack2 == 'a':
-        attack3 = input('hit him again!')
-        attack4 = input('once again!')
-        if attack4 == 'a':
-            print(BADGE)    
-        else :
-            print(GAME_OVER)
-else :
-    print(GAME_OVER)
+way = second_fight()
+lastFase = last_boss()
--- hello-python/module-2/proefA/functions.py
+++ hello-python/module-2/proefA/functions.py
@@ -1,65 +1,111 @@
 from spel_tekens import *
+
 def welcome():
-    print('welcome to dino game')
+    print('Welcome to Dino game!')
     story = 0
     while story == 0:
-            age = input('are you old enough (18) to play? (yes/no) ')
-            if age == 'no':
-                print('then what are you doing here? try again when u are old enough')
-                exit()
-            elif age == 'yes':
-                print('ur good to go')
-                story+=1
-            else :
-                print('try again!')
+        age = input('Are you old enough (18) to play? (yes/no) ')
+        if age.lower() == 'no':
+            print('Then what are you doing here? Try again when you are old enough.')
+            exit()
+        elif age.lower() == 'yes':
+            print('You are good to go!')
+            story += 1
+        else:
+            print('Please enter "yes" or "no".')
     return story
 
-#################### 
 def choose_weapon():
-    print('this story contains several bosses. to end the game you need to walk through the story and kill the minions and level up!')
-    print('if u wanna proceed in the game u need to choose a weapon:')
+    print('This story contains several bosses. To end the game, you need to walk through the story, kill the minions, and level up!')
+    print('If you want to proceed in the game, you need to choose a weapon:')
     print('sword')
     print('shield')
     print('dagger')
     weapon = 'none'
-    while not weapon in ['sword','shield','dagger']:
-            weapon = input('wich one do u choose? ')
-            if  weapon in ["sword"]:
-                print(f'oeeehhh sword let them fear the true samurai inside u ')
-            elif weapon in ['shield']:
-                print('shield is trash u will die')
-            elif weapon in ['dagger']:
-                print('wise choice')
-            else:
-                print('thats not mentioned')
+    while weapon not in ['sword', 'shield', 'dagger']:
+        weapon = input('Which one do you choose? ')
+        if weapon == 'sword':
+            print(f'Oeeehhh sword! Let them fear the true samurai inside you!')
+        elif weapon == 'shield':
+            print('Shield is trash. You will die.')
+        elif weapon == 'dagger':
+            print('Wise choice!')
+        else:
+            print('That\'s not mentioned.')
     return weapon
 
-##########################
 def ready_to_fight():
-    begin = True
-    while begin == True:
-            begin = input('now you can begin fighting, this is a miniboss, do you want to proceed? (yes/no) ')
-            if begin == 'no':
-                print('try again when ur ready: BE A MAN')
-                begin == False
-                exit()
-            elif begin == 'yes':
-                print('may the gods protect you')
-                begin == True
-                print('\n')
-            else :
-                print('Wrong answer buddy')
+    while True:
+        begin = input('Now you can begin fighting. This is a miniboss. Do you want to proceed? (yes/no) ')
+        if begin.lower() == 'no':
+            print('Try again when you are ready: BE A MAN!')
+            exit()
+        elif begin.lower() == 'yes':
+            print('May the gods protect you!')
+            print('\n')
+            return False
+        else:
+            print('Please enter "yes" or "no".')
 
-            return begin
-##########################
 def first_fight():
     print(DINO1)
-    attack == True
+    attack = True
     while attack == True:
-            attack = input('press e to attack!')
-            if attack == 'e':
-                print('what a hit! nice job, you will proceed to the next level')
-            else:
-                attack == False
-                print(GAME_OVER)
+        attack = input('Press "e" to attack!')
+        if attack.lower() == 'e':
+            print('What a hit! Nice job! You will proceed to the next level.')
+            print('\n')
+        else:
+            attack = False
+            print(GAME_OVER)
     return attack
+
+def second_fight():
+    story = 1
+    while story < 3:
+        way = input('''Now you are at a crossway. You have to choose to go left or right!
+        Where do you want to go? Choose a direction! But you know there are consequences for which way you choose... ''')
+        if way.lower() == 'left':
+            print('You are now in the jungle. Here you will play against the pro old sexy trex king. This king is not easy; you will struggle!')
+            ready = input('Are you ready? (yes/no) ')
+            if ready.lower() == 'yes':
+                print('Let the game begin!')
+                story += 1
+            elif ready.lower() == 'no':
+                print('Yeah, no problem. You will get over it. Just run!')
+            else:
+                print('Please enter "yes" or "no".')
+            print(DINO2)
+            attack1 = input('Press "s" to attack!')
+            if attack1.lower() == 's':
+                print("That was a good hit! Let's go!")
+            else:    
+                print(GAME_OVER)
+        elif way.lower() == 'right':
+                print('you are right, right is the right answer nou you can skip miniboss to go to the last boss!')
+                story+=1
+                print(DINO3)
+                skip = input('press x to skip')
+                if skip == "x":
+                    print('Now you are in the last fase, you have to defeat the last boss to win the game!')
+        else:
+            print('thats not a direction')    
+    print('\n')
+    return way
+
+#last boss
+def last_boss():
+    lastFase = input('Hello mighty dino slayer, this is the last fase, now u will fase the true fear! press x to enter the room')
+    if lastFase =="x":
+        print(BOSS)
+        attack2 = input('this is the true fear! press a to attack!')
+        if attack2 == 'a':
+            attack3 = input('hit him again!')
+            attack4 = input('once again!')
+            if attack4 == 'a':
+                print(BADGE)    
+            else :
+                print(GAME_OVER)
+    else :
+        print(GAME_OVER)
+    return lastFase
