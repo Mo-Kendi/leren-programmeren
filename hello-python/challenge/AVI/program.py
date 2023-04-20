a750edb9d1fc347205bda43d74210c0fa8e55d05
Mohamad Kendi <99072874@mydavinci.nl>
2023-04-20 13:16:07 +0200

    opdracht 4 van de challenge

--- hello-python/challenge/AVI/program.py
+++ hello-python/challenge/AVI/program.py
@@ -6,8 +6,10 @@ import tkinter.messagebox # if you want to send messages to the user.
 # functions
 def calculate():
     textToBeCalculated = calculateInput.get('1.0', 'end') # get all the text from inputfield
-    characterLabel.config(text=f"Zinnen: {getNumberOfCharacters(textToBeCalculated)}")
+    characterLabel.config(text=f"letters: {getNumberOfCharacters(textToBeCalculated)}")
     sentencesLabel.config(text =f"Zinnen: {getNumberOfSentences(textToBeCalculated)}")
+    wordLabel.config(text =f"woorden: {getNumberOfWords(textToBeCalculated)}")
+
 
 
 #variables TK
@@ -16,13 +18,16 @@ root.title('Text analyser') # set title
 root.geometry('600x600')    # set dimension
 calculateInput = tk.Text(root, width = 70, height = 30, background='lightgrey')            # generate imput element
 calculateButton = ttk.Button(root, text='Bereken score(s)', command=calculate)        # generate button when pressed -> calculate
-characterLabel = tk.Label(root, text=f'Karakters:', width=20, bg='black', fg='white') # generate characterLabel
-sentencesLabel = tk.Label(root, text=f'Zinnen:', width=20, bg='black', fg='white')    # generate sentencesLabel
+characterLabel = tk.Label(root, text=f'Karakters:', width=10, bg='black', fg='white') # generate characterLabel
+sentencesLabel = tk.Label(root, text=f'Zinnen:', width=10, bg='black', fg='white')    # generate sentencesLabel
+wordLabel = tk.Label(root, text=f'woorden:', width=10, bg='black', fg='white')    # generate woordlebel
+
 
 calculateInput.place(x=20, y=20)   # place is one of the ways to put elements on root (window).
 calculateButton.place(x=20, y=520)
-characterLabel.place(x=180, y=560)
+characterLabel.place(x=160, y=560)
 sentencesLabel.place(x=20, y=560)
+wordLabel.place(x=300, y=560)
 
 # start program
 root.mainloop() # runs until stopped with default stop button.
